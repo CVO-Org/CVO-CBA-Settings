5d527c3226ff4c95c498dc07f4e320884966c1e4
OverlordZorn <56258612+OverlordZorn@users.noreply.github.com>
2024-09-21 20:28:05 +0200

    Create config.cpp

---
+++ addons/cba_settings_userconfig/config.cpp
@@ -0,0 +1,17 @@
+
+class CfgPatches {
+    class cba_settings_userconfig {
+        author = "Overlord Zorn [CVO]";
+        name = "CVO CBA Settings Addon";
+        url = "http://discord.chornivoron.net";
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.0;
+        requiredAddons[] = {"cba_settings"};
+        version = 1.0;
+        authors[] = {"commy2"};
+    };
+};
+
+// Uncommenting this will make any changes to "Server" settings be lost upon game restart, applies only to dedicated servers
+// cba_settings_volatile = 1;
