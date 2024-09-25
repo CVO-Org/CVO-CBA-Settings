a064b3b68e83e9b4d671836e255824535357e00b
OverlordZorn <56258612+OverlordZorn@users.noreply.github.com>
2024-09-25 18:28:51 +0200

    init2

---
+++ mod.cpp
@@ -0,0 +1,33 @@
+name = "CVO CBA Settings";
+
+author = "Overlord Zorn";
+
+picture         = "img\flag_voron_red_1024_2048.paa";                                                                                               // Picture displayed from the expansions menu. Optimal size is 2048x1024
+hideName        = "false";                                                                                                                          // Hide the extension name
+hidePicture     = "false";                                                                                                                          // Hide the extension menu
+
+logoSmall       = "img\Raven_Voron_64.paa";                                                                                                    // Display next to the item added by the mod 64x64
+logo            = "img\logo-voron-white_128.paa";                                                                                                   // Logo displayed in the main menu 128x128
+logoOver        = "img\logo-voron-red_128.paa";                                                                                                     // When the mouse is over, in the main menu 128x128
+
+tooltip         = "CVO CBA Settings";
+tooltipOwned    = "Who sold this to you?";                                                                                                          // Tool tip displayed when the mouse is left over, in the main menu
+overview        = "CVO CBA Settings - Contains only our CBA Settings. You really dont need that mod loaded!";                 // Overview text, displayed from the extension menu
+description     = "Why you have this loaded bro?!";                                                     // Probably in context with action
+action          = "http://discord.chornivoron.net/";                                                                                                // Website URL, that can accessed from the expansions menu 
+actionName      = "Discord";                                                                                                                        // label of button/tooltip in extension menu
+
+overviewPicture = "voron_round_olive_texture_1024.paa";
+overviewText = "CVO CBA Settings";
+
+overviewFootnote = "CVO CBA Settings";
+
+
+// Color used for DLC stripes and backgrounds (RGBA)
+dlcColor[] =
+{
+    0.4117647058823529,
+    0.0,
+    0.0,
+    1
+};
