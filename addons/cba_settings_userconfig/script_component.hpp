e14659ad1b6f619483c92d553b962e9de4cd28be
OverlordZorn <56258612+OverlordZorn@users.noreply.github.com>
2024-10-25 16:25:00 +0200

    Create script_component.hpp

---
+++ addons/cba_settings_userconfig/script_component.hpp
@@ -0,0 +1,6 @@
+#define COMPONENT cba_settings_userconfig
+#define COMPONENT_BEAUTIFIED CVOs Serverside CBA Settings       // This is being used for the Addon's Name and can be "My Addon Template Framework"
+
+
+#include "\z\cvo-set\addons\main\script_mod.hpp"
+#include "\z\cvo-set\addons\main\script_macros.hpp"
