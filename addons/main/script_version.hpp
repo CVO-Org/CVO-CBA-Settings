9948b1ddb13fa1fcb2bd600e92c2bbef4dbbcc28
OverlordZorn <56258612+OverlordZorn@users.noreply.github.com>
2024-11-01 17:56:06 +0100

    meta

--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,5 +1,10 @@
-#define MAJOR  1
+#define MAJOR 1
 #define MINOR 1
 #define PATCH 0
 #define BUILD 1
-#define COMMIT empty
+
+
+// #define VERSION MACROS
+#define VERSION MAJOR.MINOR
+#define VERSION_AR MAJOR,MINOR,PATCH,BUILD
+#define VERSION_STR MAJOR##.##MINOR##.##PATCH##.##BUILD
