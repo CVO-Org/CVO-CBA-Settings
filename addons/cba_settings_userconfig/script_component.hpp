e390db0bb79a7f66fe4188db26cf683d8c3ce788
OverlordZorn <dennisAzorn@gmail.com>
2024-12-02 18:10:59 +0100

    mainprefix z -> zrn

--- addons/cba_settings_userconfig/script_component.hpp
+++ addons/cba_settings_userconfig/script_component.hpp
@@ -2,5 +2,5 @@
 #define COMPONENT_BEAUTIFIED CVOs Serverside CBA Settings       // This is being used for the Addon's Name and can be "My Addon Template Framework"
 
 
-#include "\z\cvo_set\addons\main\script_mod.hpp"
-#include "\z\cvo_set\addons\main\script_macros.hpp"
+#include "\zrn\cvo_set\addons\main\script_mod.hpp"
+#include "\zrn\cvo_set\addons\main\script_macros.hpp"
